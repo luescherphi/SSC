150fac626a54b4164fa71f0242e2fc4323034a35
Julian Amberg <julian.amberg@ict-campus.net>
2019-04-18 13:10:36 +0200

    Test Push from IntelliJ 3

---
+++ test/ausprobieren/SocketClientTest.java
@@ -0,0 +1,28 @@
+package ausprobieren;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SocketClientTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void startConnection() {
+    }
+
+    @Test
+    void sendMessage() {
+    }
+
+    @Test
+    void stopConnection() {
+    }
+
+    @Test
+    void inputMessage() {
+    }
+}
